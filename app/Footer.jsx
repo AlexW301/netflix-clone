3b5f00034e3dd3bd733c723e34790073791e8d7e
AlexW301 <66179529+AlexW301@users.noreply.github.com>
2023-08-04 22:26:57 -0400

    okay final edit

--- app/Footer.jsx
+++ app/Footer.jsx
@@ -2,6 +2,6 @@ import React from 'react'
 
 export default function Footer() {
   return (
-    <div style={{width: "100vw", display: "flex", alignItems: "center", justifyContent: "center", padding: "40px"}}>Developed by Alex Waller</div>
+    <footer style={{width: "100vw", display: "flex", alignItems: "center", justifyContent: "center", padding: "40px"}}>Developed by Alex Waller</footer>
   )
 }
--- app/Header.jsx
+++ app/Header.jsx
@@ -4,8 +4,10 @@ import Link from 'next/link'
 
 export default function Header() {
   return (
-    <Link href={`/`} style={{width: "100vw", display: "flex", alignItems: "center", justifyContent: "center"}}>
-      <Image src={`/netflix-logo.png`} alt="Netflix Logo" width={700 / 3.5} height={419 / 3.5} />
-    </Link>
+    <header style={{ width: "100vw", display: "flex", alignItems: "center", justifyContent: "center" }}>
+      <Link href={`/`}>
+        <Image src={`/netflix-logo.png`} alt="Netflix Logo" width={700 / 3.5} height={419 / 3.5} />
+      </Link>
+    </header>
   )
 }
